b07dfa76b200a96723c30a1633a8729897eeeebd
Oliviaparkerva <31199853+Oliviaparkerva@users.noreply.github.com>
2018-10-18 01:44:52 -0400

    created tokein
    Created MAPBOX  API Key inserted into js files

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoib3B2YSIsImEiOiJjam5iYnZuaWowNXVuM3BwY2ZucDliYjB2In0.cZCWqIa1MAYfwYXUExYTBA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
