cc6acd22d173b60e570b88deb7ad2df0dad3dd6e
Oliviaparkerva <31199853+Oliviaparkerva@users.noreply.github.com>
2018-10-23 16:58:47 -0400

    updated service worker activate
    added activate event listener

--- sw.js
+++ sw.js
@@ -1,44 +1,52 @@
 //https://css-tricks.com/serviceworker-for-offline/
+let staticCacheName = 'restaurant-reviews-v1';
 
-let cacheVersion = 'restaurant-review-v1';
-
-self.addEventListener('install', function(e) {
-	e.waitUntil(
-		caches.open(cacheVersion).then(function(cache){
-		return cache.addAll([
-	'/',
-	'/index.html',
-	'/restaurant.html',
-	'/css/styles.css',
-	'/data/restaurants.json',
-	'/img/1.jpg',
-	'/img/2.jpg',
-	'/img/3.jpg',
-	'/img/4.jpg',
-	'/img/5.jpg',
-	'/img/6.jpg',
-	'/img/7.jpg',
-	'/img/8.jpg',
-	'/img/9.jpg',
-	'/img/10.jpg',
-	'/js/dbhelper.js',
-	'/js/main.js',
-	'/js/restaurant_info.js'
-]);
-		})
-		.then(function(){
-			console.log('Cache opened, install complete');
-
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open(staticCacheName).then(function(cache){
+			return cache.addAll([
+				'/',
+				'/index.html',
+				'/restaurant.html',
+				'/css/styles.css',
+				'/data/restaurants.json',
+				'/img/1.jpg',
+				'/img/2.jpg',
+				'/img/3.jpg',
+				'/img/4.jpg',
+				'/img/5.jpg',
+				'/img/6.jpg',
+				'/img/7.jpg',
+				'/img/8.jpg',
+				'/img/9.jpg',
+				'/img/10.jpg',
+				'/js/dbhelper.js',
+				'/js/main.js',
+				'/js/restaurant_info.js'
+			]);
 		})
 	);
 });
-//https://is.gd/tcwIup
-self.addEventListener('fetch', function(e) {
-	console.log(e.request.url);
-	e.respondWith(
-		caches.match(e.request).then(function(response) {
-			return response || fetch(e.request);
+
+self.addEventListener('fetch', function(event){
+	event.respondWith(
+		caches.match(event.request).then(function(response){
+			if(response)return response;
+			return fetch(event.request);
 		})
 	)
 });
 
+self.addEventListener('activate', function(event){
+	event.waitUntil(
+		caches.keys().then(function(cacheNames){
+			return Promise.all(
+				cacheNames.filter(function(cacheName){
+					return cacheName != staticCacheName
+				}).map(function(cacheName){
+					return cache.delete(cacheName);
+				})
+			);
+		})
+	);
+});
