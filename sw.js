6e221e1981e9dce2416b7058ff2800805c1e5028
Oliviaparkerva <31199853+Oliviaparkerva@users.noreply.github.com>
2018-10-24 14:16:28 -0400

    Aria Roles alt tags offline functionality
    Added proper aria roles and labels to html files. Added alt image tags to restaurant images via. Corrected bug in service worker using clone() to supply both cache and browser responses and request.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 //https://css-tricks.com/serviceworker-for-offline/
-let staticCacheName = 'restaurant-reviews-v1';
+var staticCacheName = 'restaurant-reviews-v1';
 
 self.addEventListener('install', function(event) {
 	event.waitUntil(
@@ -28,25 +28,52 @@ self.addEventListener('install', function(event) {
 	);
 });
 
-self.addEventListener('fetch', function(event){
-	event.respondWith(
-		caches.match(event.request).then(function(response){
-			if(response)return response;
-			return fetch(event.request);
-		})
-	)
-});
-
 self.addEventListener('activate', function(event){
 	event.waitUntil(
-		caches.keys().then(function(cacheNames){
-			return Promise.all(
-				cacheNames.filter(function(cacheName){
-					return cacheName != staticCacheName
-				}).map(function(cacheName){
-					return cache.delete(cacheName);
-				})
-			);
-		})
+		caches.keys()
+			.then(function(cachesNames){
+				return Promise.all(
+					cachesNames.filter(function(cacheName){
+					return cacheName.startsWith('restaurant-') && cacheName != staticCacheName;
+					})
+					.map(function(cacheName){
+						return caches.delete(cacheName);
+					})
+				);
+			})
 	);
 });
+
+//https://is.gd/LSMciS
+//https://is.gd/RA0ZEx example of clone method in walkthrough
+//https://is.gd/m7hcaX Fetch aPI request.clone() documentation
+self.addEventListener('fetch', function(event){
+	event.respondWith(
+		caches.match(event.request)
+			.then(function(response){
+				if(response){
+					return response;
+				}
+				//first request was for the cache one time use
+				var fetchRequest = event.request.clone();
+				//valid response? 200 means ok
+				return fetch(fetchRequest)
+					.then(function(response){
+						if (!response || response.status !== 200 || response.type !=='basic'){
+							return response;
+						}
+						var cacheResponse = response.clone();
+						//first response goes to browser and is done, this response is for the cache
+						caches.open(staticCacheName)
+							.then(function(cache){
+								cache.put(event.request, cacheResponse);
+							});
+					
+					return response;
+					
+					});
+			})
+	);
+});
+
+
